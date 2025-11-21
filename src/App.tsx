f8888b00a7af9bd70d5fdfdbd330c58caf889a6e
Martiniano Gomes <148648246+MartinianoGomes@users.noreply.github.com>
2025-11-21 13:55:08 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { Routes, Route } from "react-router"
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/gymplanner" element={<div>Gymplanner</div>} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/gymplanner" element={<div />} />
+      <Route path="/gymplanner/login" element={<div />} />
+      <Route path="/gymplanner/register" element={<div />} />
+      <Route path="/gymplanner/me" element={<div />} />
+    </Routes>
   )
 }
 
