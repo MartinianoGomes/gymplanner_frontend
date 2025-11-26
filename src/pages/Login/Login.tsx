80de5170fb5202cacfe31d300c64c8ae74e857ac
Martiniano Gomes <148648246+MartinianoGomes@users.noreply.github.com>
2025-11-26 16:30:27 -0300

    merge

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,46 @@
+import { useNavigate } from 'react-router';
+import { useState } from 'react';
+
 import logoImage from '../../assets/login/logo-1.svg';
+import { api } from '../../services/api/api';
+import { AxiosError } from 'axios';
 
 export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        try {
+            const data = { email, password };
+
+            const response = await api.post('/auth/login', data);  // cookies incluídos
+
+            alert(response.data.message ?? "Login feito com sucesso!");
+
+            const user = await api.get('/me');
+
+            console.log(JSON.stringify(user.data));
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                const status = error.response?.status;
+                const backendErr = error.response?.data?.error;
+
+                switch (status) {
+                    case 400:
+                        return alert("Credenciais inválidas.");
+                    case 401:
+                        return alert("E-mail ou senha incorretos.");
+                }
+
+                return alert(backendErr ?? "Erro inesperado ao fazer login.");
+            }
+
+            alert("Erro inesperado.");
+        }
+    }
+
     return (
         <div className="grid min-h-screen w-full md:grid-cols-2">
             <div className="flex flex-col justify-center m-auto px-3 min-w-[80%] bg-white md:max-w-[80%] md:justify-center">
@@ -14,7 +54,7 @@ export default function Login() {
                     Faça login na sua conta e aproveite todos os benefícios gratuitamente.
                 </p>
 
-                <form className="flex flex-col">
+                <form onSubmit={onSubmit} className="flex flex-col">
                     <label htmlFor="email" className="block text-gray-700 font-medium mb-2 text-sm">
                         E-mail
                     </label>
@@ -23,6 +63,7 @@ export default function Login() {
                         id="email"
                         placeholder="Digite seu e-mail"
                         required
+                        onChange={(e) => setEmail(e.target.value)}
                         className="w-full px-4 py-3 bg-gray-100 border-none rounded-lg text-gray-800 text-sm transition-all mb-6 placeholder:text-gray-400 focus:outline-none focus:bg-white focus:shadow-[0_0_0_2px_#f97316]"
                     />
 
@@ -34,6 +75,7 @@ export default function Login() {
                         id="password"
                         placeholder="Digite sua senha"
                         required
+                        onChange={(e) => setPassword(e.target.value)}
                         className="w-full px-4 py-3 bg-gray-100 border-none rounded-lg text-gray-800 text-sm transition-all mb-6 placeholder:text-gray-400 focus:outline-none focus:bg-white focus:shadow-[0_0_0_2px_#f97316]"
                     />
 
@@ -46,7 +88,7 @@ export default function Login() {
 
                     <p className="text-[13px] text-center text-gray-600">
                         Não possui conta?{' '}
-                        <a href="/cadastro" className="no-underline text-orange-500 hover:underline">
+                        <a href="/register" className="no-underline text-orange-500 hover:underline">
                             Clique aqui e realize seu cadastro
                         </a>
                     </p>
