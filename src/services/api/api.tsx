fb7e07b18d34d736a498f95561cccf6bdc5dc80e
Martiniano Gomes <148648246+MartinianoGomes@users.noreply.github.com>
2025-11-26 16:09:04 -0300

    Revert "Update api.tsx"
    This reverts commit 80964e3f61ddb87bda8137852416e885de5cc458.

--- src/services/api/api.tsx
+++ src/services/api/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://gymplanner-api.vercel.app/",
+    baseURL: "http://localhost:3000/",
     withCredentials: true
 })
