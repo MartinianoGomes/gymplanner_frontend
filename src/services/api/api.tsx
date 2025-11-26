cfa49e2281228b2b467bd2cd12c228935b9ba2fc
Martiniano Gomes <148648246+MartinianoGomes@users.noreply.github.com>
2025-11-26 16:32:01 -0300

    Update api.tsx

--- src/services/api/api.tsx
+++ src/services/api/api.tsx
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-<<<<<<< HEAD
     baseURL: "https://gymplanner-api.vercel.app/",
-=======
-    baseURL: "http://localhost:3000/",
->>>>>>> parent of c496033 (Revert "feat/apiIntegration")
     withCredentials: true
 })
