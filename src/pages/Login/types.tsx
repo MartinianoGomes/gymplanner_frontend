fd3e63ea2d976515c542b1d66230664d1a960a93
Martiniano Gomes <148648246+MartinianoGomes@users.noreply.github.com>
2025-11-29 12:32:57 -0300

    Update validations with yup

--- src/pages/Login/types.tsx
+++ src/pages/Login/types.tsx
@@ -1,4 +1,8 @@
-export default interface LoginFormInputs {
-    email: string;
-    password: string;
-}
+import * as yup from "yup";
+
+export const loginFormSchema = yup.object({
+    email: yup.string().email("E-mail inválido!").required("E-mail é obrigatório!"),
+    password: yup.string().required("Senha é obrigatória!").min(8, "Senha deve ter no mínimo 8 caracteres."),
+})
+
+export type LoginFormData = yup.InferType<typeof loginFormSchema>;
