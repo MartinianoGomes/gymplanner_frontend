561e7d02c8380e494670ab1d517dd48aba6f0292
Martiniano Gomes <148648246+MartinianoGomes@users.noreply.github.com>
2025-11-29 12:32:40 -0300

    Configuring the sonner

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router"
 import Login from "./pages/Login/Login"
 import Register from "./pages/Register/Register"
+import { Toaster } from "sonner"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <>
+      <Toaster richColors position="top-left" />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   )
 }
 
