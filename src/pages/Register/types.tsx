c9c9ccdb0933cc0caddc6131c3624c823a0411aa
Martiniano Gomes <148648246+MartinianoGomes@users.noreply.github.com>
2025-11-29 12:51:21 -0300

    Create yup validation for register page

--- src/pages/Register/types.tsx
+++ src/pages/Register/types.tsx
@@ -1,6 +1,12 @@
-export default interface RegisterFormInputs {
-    name: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-}
+import * as yup from "yup";
+
+export const registerSchema = yup.object({
+    name: yup.string().required("Nome é obrigatório"),
+    email: yup.string().email("E-mail inválido").required("E-mail é obrigatório"),
+    password: yup.string().min(8, "Senha deve ter no mínimo 8 caracteres").required("Senha é obrigatória"),
+    confirmPassword: yup.string()
+        .oneOf([yup.ref("password")], "As senhas devem corresponder")
+        .required("Confirmação de senha é obrigatória"),
+})
+
+export type RegisterData = yup.InferType<typeof registerSchema>;
