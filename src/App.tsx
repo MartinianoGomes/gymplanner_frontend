da15732ac8301be7f36c32161034dc18c02d3599
Martiniano Gomes <148648246+MartinianoGomes@users.noreply.github.com>
2025-12-10 21:57:39 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import { Routes, Route } from "react-router"
+import { Routes, Route, useLocation } from "react-router"
 import Login from "./pages/Login/Login"
 import Register from "./pages/Register/Register"
 import { Toaster } from "sonner"
 import Test from "./pages/Test"
+import NavMenu from "./components/NavMenu"
+
 
 function App() {
+  const location = useLocation();
+  const showNavMenu = location.pathname !== '/login' && location.pathname !== '/register' && location.pathname !== '/';
+
   return (
-    <>
+    <div className="min-h-screen">
       <Toaster richColors position="top-left" />
+      {showNavMenu && <NavMenu />}
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/test" element={<Test />}></Route>
       </Routes>
-    </>
+    </div>
   )
 }
 
